0d3cb2ef31350a9a11c329c0bce6ad2b6c3adca7
Emma Egstad <emma@egstad.com>
2022-01-10 12:19:01 -0800

    creating file structure

---
+++ src/views/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Auth.css';
+import AuthForm from '../components/AuthForm';
+
+export default function Auth() {
+  return (
+    <div className="Auth">
+      <AuthForm />
+    </div>
+  );
+}
